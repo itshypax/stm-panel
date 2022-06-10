ff6db7c3c1dbd9000f37cb91ad60d10aba867256
hypax <me@hypax.wtf>
2022-06-11 00:09:32 +0200

    new staff

--- assets/components/fb37allowedids.php
+++ assets/components/fb37allowedids.php
@@ -2,8 +2,9 @@
 
 $allowed_steamids = array(
     '76561198177423823', // James van Price
-    '76561198301210949' // Emilio Castellano
+    '76561198301210949', // Emilio Castellano
     //'76561198110429932' // Hypax Testaccount
+    '76561198111831900' // Nico Johnson 
 );
 
 ?>
