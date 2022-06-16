b8e293a6f391c278a9721d625d936c2ca2d9f373
hypax <me@hypax.wtf>
2022-06-16 14:57:38 +0200

    added metas

---
+++ assets/components/fb37meta.php
@@ -0,0 +1,7 @@
+<meta name="theme-color" content="#df691a">
+<meta property="og:site_name" content="wiesberg.net">
+<meta property="og:url" content="https://wiesberg.net/">
+<meta property="og:title" content="Straßenmeisterei Neuberg">
+<meta property="og:image" content="https://wiesberg.net/assets/images/tcMPe2F2.png">
+<meta property="og:description" content="">
+<meta name="twitter:card" content="https://wiesberg.net/assets/images/fb37dashboard.jpg">
