2f442287b5cbd9735c6915397cf79f7f157425c1
hypax <me@hypax.wtf>
2022-07-15 02:57:39 +0200

    changed file naming

--- uploader-ajax.php
+++ uploader-ajax.php
@@ -10,10 +10,11 @@ if (!file_exists('fileupload')) {
     mkdir('fileupload', 0777);
 }
   
-$filename = time().'_'.$_FILES['file']['name'];
+$filename = $_FILES['file']['name'];
   
 move_uploaded_file($_FILES['file']['tmp_name'], 'fileupload/'.$filename);
   
 echo 'fileupload/'.$filename;
 sleep(2);
+header("Location: https://strassenmeisterei-neuberg.de/fileupload/$filename"); 
 die;
