b00f04e227ad27c9c50c7f520f71ce4ce4a6e4d0
hypax <me@hypax.wtf>
2022-07-15 02:28:54 +0200

    Working on fileupload
    #49

--- uploader-ajax.php
+++ uploader-ajax.php
@@ -7,12 +7,12 @@ if (!(in_array($_FILES['file']['type'], $arr_file_types))) {
 }
   
 if (!file_exists('fileupload')) {
-    mkdir('../../fileupload', 0777);
+    mkdir('fileupload', 0777);
 }
   
 $filename = time().'_'.$_FILES['file']['name'];
   
-move_uploaded_file($_FILES['file']['tmp_name'], '../../fileupload/'.$filename);
+move_uploaded_file($_FILES['file']['tmp_name'], 'fileupload/'.$filename);
   
-echo '../../uploads/'.$filename;
+echo 'fileupload/'.$filename;
 die;
