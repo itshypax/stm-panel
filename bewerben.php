df11cb654a3eca6003b4b7029a48e394643453f4
hypax <me@hypax.wtf>
2022-07-18 18:45:19 +0200

    link should now open in new tab

--- bewerben.php
+++ bewerben.php
@@ -197,7 +197,7 @@ while ($rows = mysqli_fetch_array($dbquery)) {
               <li>Eine kreative, überzeugende Bewerbung. Wir sind für alles offen - alle Texte die uns überzeugen haben eine gute Chance bei uns aufgenommen zu werden.</li>
             </ul>
 
-            <p>Interessenten sollten ebenfalls unsere <a href="https://docs.google.com/document/d/16hvXf7KRhOcJUWDcBLXsAthDNOvRfrh8s83BhzYUSus/edit">Dienstverordnung</a> gelesen, verstanden und akzeptiert haben.</p>
+            <p>Interessenten sollten ebenfalls unsere <a href="https://docs.google.com/document/d/16hvXf7KRhOcJUWDcBLXsAthDNOvRfrh8s83BhzYUSus/edit" target="_blank">Dienstverordnung</a> gelesen, verstanden und akzeptiert haben.</p>
             <div class="form-check form-switch">
               <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" required>
               <label class="form-check-label" for="flexSwitchCheckDefault">Ich bestötige die Dienstverordnung gelesen zu haben.</label>
