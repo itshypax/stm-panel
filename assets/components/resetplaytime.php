9be7a22ef1a9641c453d8b94cccf680378dfeff8
hypax <me@hypax.wtf>
2022-07-18 17:09:56 +0200

    modified weekly playtime reset

--- assets/components/resetplaytime.php
+++ assets/components/resetplaytime.php
@@ -8,8 +8,15 @@ if ($dbconnect->connect_error) {
 	die("Fehler, Verbindung fehlgeschlagen:" . $dbconnect->connect_error);
 }
 
+$queryt = mysqli_query($dbconnect, "TRUNCATE table OldPlaytimes")
+		or die (mysqli_error($dbconnect));
+
+		sleep(60);
+
 $querya = mysqli_query($dbconnect, "INSERT INTO OldPlaytimes (name, playtime) SELECT name, playtime FROM UserPlaytimes")
 		or die (mysqli_error($dbconnect));
+
+		sleep(60);
 
 $query = mysqli_query($dbconnect, "UPDATE UserPlaytimes SET playtime='0'")
 		or die (mysqli_error($dbconnect));
