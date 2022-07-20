6261273517b653003d7bef64e8f89620accf02f8
hypax <me@hypax.wtf>
2022-07-20 17:46:37 +0200

    untest ^^

--- assets/steamauth/SteamConfig.php
+++ assets/steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 4.0
 $steamauth['apikey'] = "6B729D70516E320DC2DC2A83860133E1"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "test.strassenmeisterei-neuberg.de"; // The main URL of your website displayed in the login page
+$steamauth['domainname'] = "strassenmeisterei-neuberg.de"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
