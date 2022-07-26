ac8f2365b5e2bb652dcd50f726f4872c57e4d84d
hypax <me@hypax.wtf>
2022-07-26 17:43:04 +0200

    Testing resetplaytime

--- assets/components/resetplaytime.php
+++ assets/components/resetplaytime.php
@@ -2,21 +2,18 @@
 
 include("fb37dbconnect.php");
 
-$dbconnect=mysqli_connect($hostname,$username,$password,$dbname);
+$dbconnect = mysqli_connect($hostname, $username, $password, $dbname);
 
 if ($dbconnect->connect_error) {
 	die("Fehler, Verbindung fehlgeschlagen:" . $dbconnect->connect_error);
 }
 
-$queryt = mysqli_query($dbconnect, "TRUNCATE table OldPlaytimes")
-		or die (mysqli_error($dbconnect));
+mysqli_query($dbconnect, "TRUNCATE table OldPlaytimes") or die(mysqli_error($dbconnect));
 
-		sleep(60);
+sleep(60);
 
-$querya = mysqli_query($dbconnect, "INSERT INTO OldPlaytimes (name, playtime) SELECT name, playtime FROM UserPlaytimes")
-		or die (mysqli_error($dbconnect));
+mysqli_query($dbconnect, "INSERT INTO OldPlaytimes (name, playtime) SELECT name, playtime FROM UserPlaytimes") or die(mysqli_error($dbconnect));
 
-		sleep(60);
+sleep(60);
 
-$query = mysqli_query($dbconnect, "UPDATE UserPlaytimes SET playtime='0'")
-		or die (mysqli_error($dbconnect));
+// mysqli_query($dbconnect, "UPDATE UserPlaytimes SET playtime='0'") or die(mysqli_error($dbconnect));
